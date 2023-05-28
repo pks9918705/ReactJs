41f32e9bab61f609bb624daab3b4ab6d51df87b5
Priyanshu Kumar Singh <pks9918705@gmail.com>
2023-05-29 00:51:54 +0530

    DAY#1-- about react cdn

---
+++ Basic /index.js
@@ -0,0 +1,13 @@
+
+//! Rect with js 
+// const reactHeading= React.createElement("h1", {
+     
+//     id:"heading",
+//     children:"Hi, Priyanshu Kumar Singh"
+//   });
+
+//   ReactDOM.createRoot(document.getElementById("root")).render(reactHeading);
+
+//! React with JSX
+const jsxHeading= (<h1>Hello JSX!!</h1>)
+ReactDOM.createRoot(document.getElementById("root")).render(jsxHeading);
