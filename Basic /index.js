db3d05622b4b492cde1e5aef1095aa4b3d1cb304
Priyanshu Kumar Singh <pks9918705@gmail.com>
2023-05-29 08:42:38 +0530

    React component is created

--- Basic /index.js
+++ Basic /index.js
@@ -1,41 +1,25 @@
-
-//! Rect with js 
-// const reactHeading= React.createElement("h1", {
-
-//     id:"heading",
-//     children:"Hi, Priyanshu Kumar Singh"
-//   });
-
-//   ReactDOM.createRoot(document.getElementById("root")).render(reactHeading);
-
-//! React with JSX
+ 
+//! Component in React
 const jsxHeading =
-//? method 1- React Fragment
-// (<React.Fragment>
-//     <h1>
-//         Hello JSX!!
-//     </h1>
-//     <p>
-//         this is the paragraph
-//     </p>
 
-// </React.Fragment>
-// )
-//? method 2- Empty fragment
-(<>
-    <h1>
-        Hello JSX!!
-    </h1>
-    <p>
-        this is the paragraph
-        <ul>
-            <li>About</li>
-            <li>Contact Us</li>
-            <li>Docs     </li>
-        </ul>
-    </p>
+//react component
+function App(){
+    return <>
+        <h1>
+            Hello JSX!!
+        </h1>
+        <p>
+            this is the paragraph
+            <ul>
+                <li>About</li>
+                <li>Contact Us</li>
+                <li>Docs     </li>
+            </ul>
+        </p>
+    
+    </>
+}
 
-</>
-)
-ReactDOM.createRoot(document.getElementById("root")).render(jsxHeading);
+
+ReactDOM.createRoot(document.getElementById("root")).render(App());
 
