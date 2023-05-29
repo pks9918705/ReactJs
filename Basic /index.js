75ec9ada04a4dc58454b5aeac43349b7c6377b07
Priyanshu Kumar Singh <pks9918705@gmail.com>
2023-05-29 10:35:48 +0530

    ********************* Ch02 Learning JSX **************

--- Basic /index.js
+++ Basic /index.js
@@ -1,34 +1,36 @@
 
 //! Component in React
 
-function Name(){
-    return (
-        <>
-        <p>JSX is Javascript XML</p>
-        </>
-    )
-}
-//react Function component using Arrow Function
-const App = () => (
-     <>
-        <h1>
-            Hello JSX!!
-        </h1>
-        <p>
-            this is the paragraph
-            <ul>
-                <li>About</li>
-                <li>Contact Us</li>
-                <li>Docs     </li>
-            </ul>
-        </p>
+// function Name(){
+//     return (
+//         <>
+//         <p>JSX is Javascript XML</p>
+//         </>
+//     )
+// }
+// react Function component using Arrow Function
+// const App = () => 
+//      <>
+//         <h1>
+//             Hello JSX!!
+//         </h1>
+//         <p>
+//             this is the paragraph
+//             <ul>
+//                 <li>About</li>
+//                 <li>Contact Us</li>
+//                 <li>Docs     </li>
+//             </ul>
+//         </p>
 
-        <Name/>  
+//         <Name/>  
     
-    </>
-)
+//     </>
+
 // calling another comp fxn from other  components function
 //calling two components side by side
-ReactDOM.createRoot(document.getElementById("root")).render(  <App/>   );// called it as component
+// ReactDOM.createRoot(document.getElementById("root")).render(  <App/>   );
+// called it as component
 
- 
+//?  HomeWork 
+ReactDOM.createRoot(document.getElementById("root")).render(  <h1>this is h1</h1>  );
