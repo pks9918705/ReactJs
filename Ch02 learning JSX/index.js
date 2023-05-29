9e99477307211bd163f6940f6daee7827ed80d02
Priyanshu Kumar Singh <pks9918705@gmail.com>
2023-05-29 14:37:16 +0530

    JSX exp Done

--- Ch02 learning JSX/index.js
+++ Ch02 learning JSX/index.js
@@ -1,35 +1,41 @@
 
 
-function App(){
+function App() {
     //below return only JSX should be there
     // variable is declared here
-    var name="Priyanshu Kumar Singh"
-    var age=23
+    var name = "Priyanshu Kumar Singh"
+    var age = 23
     //indefined values and boolean value are not rendered on screen
-    var demo =null
-    var bool=false
+    var demo = null
+    var bool = false
 
     //function to embedded into JSX
-    function sum(a,b){
-        return a+b
+    function sum(a, b) {
+        return a + b
     }
 
-    return(
-    <>
-         <h1>Lets Run JSX !!</h1>
-         <p>Name: {name}</p>
-         <p>Age: {age}</p>
-         <p>demo: {demo}</p>
-         <p>Boolean: {bool}</p>
-         <p>Sum of 3 and 5: {sum(3,5)}</p>
-        </>
-       
-    )
-            
+    //it is returning JSX to header / header is js 
+    const header = <h2>JSX EXPRESSION</h2>
+    //here JSX is assigned to js and it now it is used as variable and can be access to JSX using {}
 
-    
-    
-    
+    return (
+        // returnig JSX to UI
+        <>
+            <h1>Lets Run JSX !!</h1>
+            {header}
+            <p>Name: {name}</p>
+            <p>Age: {age}</p>
+            <p>demo: {demo}</p>
+            <p>Boolean: {bool}</p>
+            <p>Sum of 3 and 5: {sum(3, 5)}</p>
+        </>
+
+    )
+
+
+
+
+
 }
 
-ReactDOM.createRoot(document.getElementById("root")).render( <App/>);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
