d1975eece9046a4f58c87a97154f5f35e0cfc55e
Priyanshu Kumar Singh <pks9918705@gmail.com>
2023-05-29 14:05:19 +0530

    indefined values and boolean value are not rendered on screen

---
+++ Ch02 learning JSX/index.js
@@ -0,0 +1,30 @@
+
+
+function App(){
+    //below return only JSX should be there
+    // variable is declared here
+    var name="Priyanshu Kumar Singh"
+    var age=23
+    //indefined values and boolean value are not rendered on screen
+    var demo =null
+    var bool=false
+
+    return(
+
+        <>
+         <h1>Lets Run JSX !!</h1>
+         <p>Name: {name}</p>
+         <p>Age: {age}</p>
+         <p>demo: {demo}</p>
+         <p>Boolean: {bool}</p>
+        </>
+       
+    )
+            
+
+    
+    
+    
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render( <App/>);
