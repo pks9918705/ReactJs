ce67f9e147b7f8b9a090390b0a3eedb4ddbda4b1
Priyanshu Kumar Singh <pks9918705@gmail.com>
2023-05-30 08:28:50 +0530

    **Important Concept**--- JSX into JS and then Js into JSX

--- Ch02 learning JSX/index.js
+++ Ch02 learning JSX/index.js
@@ -1,42 +1,51 @@
-
 //! Rendering Array and Objects
 function App() {
-
-    const a=[1,2,3,4,5,6]
-    const obj={
-        name:"Alexa",
-        age:23
-    }
-
-    const cars=["BmW","TATA","Mahindra","Fortuner"]
-
-   return (
-        // returnig JSX to UI
-        <>
-            <h1>Lets Run JSX !!</h1>
-            <p>Array is {a}</p>
-            {/* <p>Oject is {obj}</p> */} 
-            {/* object cannot be rendered , Only its key value pair can be rendered */}
-            <p>Name: {obj.name}</p>
-            <p>Age: {obj.age}</p>
-
-            {/* Elemts of Array - Line by Line  */}
-            {a.map((num)=> <h2> {num} </h2>)}
-
-            Printing unordered list
-            <ol>
-            { cars.map((car)=> <li>{car}</li>  )}
-            </ol>
-
-              
-        </>
-
-    )
-
-
-
-
-
-}
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+    // student data
+    const students = [
+      {
+        name: "Priyanshu",
+        class: 12,
+        marks: 90
+      },
+      {
+        name: "Priya",
+        class: 120,
+        marks: 900
+      },
+      {
+        name: "anshu",
+        class: 12,
+        marks: 980
+      },
+      {
+        name: "Priy88anshu",
+        class: 12,
+        marks: 908
+      }
+    ];
+  
+    return (
+      // returning JSX to UI
+      <>
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Class</th>
+            <th>Marks</th>
+          </tr>
+     
+        {/* Concept:: returning JSX to JS and then Js is imbeded into JSX  */}
+          {students.map((student) => (
+            <tr key={student.name}>
+              <td>{student.name}</td>
+              <td>{student.class}</td>
+              <td>{student.marks}</td>
+            </tr>
+          ))}
+        </table>
+      </>
+    );
+  }
+  
+  ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+  
