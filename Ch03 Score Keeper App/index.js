b7a283783a292d69d07a540d3e58cf23e361714e
Priyanshu Kumar Singh <pks9918705@gmail.com>
2023-05-30 15:18:22 +0530

    inline functional calling from jsx

---
+++ Ch03 Score Keeper App/index.js
@@ -0,0 +1,50 @@
+//! rerendering the App after change in UI- 
+// virtual DOM is working here 
+let score = 0;
+let wicket = 0;
+
+function addOne() {
+    score += 1;
+    console.log(score);
+    rootElement.render(<App />)
+}
+function addScore(s) {
+    score += s;
+    console.log(score);
+    rootElement.render(<App />)
+}
+function wicketFxn(){
+    wicket += 1;
+    rootElement.render(<App />)
+}
+//  Creating score keeper app
+function App() {
+
+    return (
+        <>
+            <h1>SCORE KEEPER</h1>
+            <h2>SCORE: {score}/{wicket}</h2>
+
+            <div>
+                <button onClick={addOne}>1</button>
+                {/* <button onClick={addScore(2)}>2</button> */}
+                {/* Above will get trapped in infinte loop  */}
+                {/* to make it work we use inline function */}
+                <button onClick={()=>addScore(2)}>2</button>
+                <button onClick={()=>addScore(3)}>3</button>
+                <button onClick={()=>addScore(4)}>4</button>
+                <button onClick={()=>addScore(5)}>5</button>
+                <button onClick={()=>addScore(6)}>6</button>
+                 
+                <button onClick={wicketFxn}>wicket</button>
+            </div>
+        </>
+    );
+}
+
+var rootElement = ReactDOM.createRoot(document.getElementById("root"))
+rootElement.render(<App />)
+
+
+
+ 
