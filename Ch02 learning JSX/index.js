a4e4a95db9e0d02cf5d5d1c36e5b92fb7359ded2
Priyanshu Kumar Singh <pks9918705@gmail.com>
2023-05-30 08:49:36 +0530

    Updated version of Table Rendering

--- Ch02 learning JSX/index.js
+++ Ch02 learning JSX/index.js
@@ -2,50 +2,60 @@
 function App() {
     // student data
     const students = [
-      {
-        name: "Priyanshu",
-        class: 12,
-        marks: 90
-      },
-      {
-        name: "Priya",
-        class: 120,
-        marks: 900
-      },
-      {
-        name: "anshu",
-        class: 12,
-        marks: 980
-      },
-      {
-        name: "Priy88anshu",
-        class: 12,
-        marks: 908
-      }
+        {
+            name: "Priyanshu",
+            class: 12,
+            marks: 90,
+             image:"./images/bg.jpg"
+        },
+        {
+            name: "Priya",
+            class: 120,
+            marks: 900,
+            image:"./images/bg.jpg"
+        },
+        {
+            name: "anshu",
+            class: 12,
+            marks: 980,
+            image:"./images/bg.jpg"
+        },
+        {
+            name: "Priy88anshu",
+            class: 12,
+            marks: 908,
+            image:"./images/bg.jpg"
+        }
     ];
-  
+
     return (
-      // returning JSX to UI
-      <>
-        <table>
-          <tr>
-            <th>Name</th>
-            <th>Class</th>
-            <th>Marks</th>
-          </tr>
-     
-        {/* Concept:: returning JSX to JS and then Js is imbeded into JSX  */}
-          {students.map((student) => (
-            <tr key={student.name}>
-              <td>{student.name}</td>
-              <td>{student.class}</td>
-              <td>{student.marks}</td>
-            </tr>
-          ))}
-        </table>
-      </>
+        // returning JSX to UI
+        <>
+            <table border="1px">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Class</th>
+                        <th>Marks</th>
+                        <th>Picture</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {/* Concept:: returning JSX to JS and then Js is imbeded into JSX  */}
+                    {students.map((student) => (
+                        <tr key={student.name}>
+                            <td>{student.name}</td>
+                            <td>{student.class}</td>
+                            <td>{student.marks}</td>
+                            <td >  <img src={student.image} alt="Girl in a jacket" width="100" height="100" />   </td>
+                        </tr>
+                    ))}
+                </tbody>
+
+            </table>
+        </>
     );
-  }
-  
-  ReactDOM.createRoot(document.getElementById("root")).render(<App />);
-  
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
