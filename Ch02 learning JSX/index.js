4540d6b5d7bd8963a6e84d7bcabccb62436ebf7e
Priyanshu Kumar Singh <pks9918705@gmail.com>
2023-05-30 11:11:23 +0530

    Updated Conditional Returning Components

--- Ch02 learning JSX/index.js
+++ Ch02 learning JSX/index.js
@@ -1,13 +1,44 @@
 //!  Returning components based on Conditional Rendering - ternary operators
-function App() {
+// function App() {
+   // student data
+//      let n=4
+
+//     return (
+       // returning JSX to UI
+//         <>
+//            <h1> Hello { (n==3)? "Alexa!!":"Priyanshu" } </h1>
+            
+//         </>
+//     );
+// }
+//!  Returning components based on Conditional Rendering - AND operators
+// function App() {
     // student data
-     let n=4
+//      let loggedIn = false;
+    //  let loggedIn = true;
+
+//     return (
+       // returning JSX to UI
+//         <>
+//             <h1>Hello {(loggedIn)?"Alexa !!":"User"}</h1>
+//              {/* since it is show the last truly value so we put 2nd para in last parameter */}
+//             <p>{ loggedIn && "Welcome Alexa to your profile"  }</p>            
+//         </>
+//     );
+// }
+//!  Returning components based on Conditional Rendering - OR operators
+function App() {
+     
+     let loggedIn = true;
+      let first="Priyanshu"
+      let lastname="Singh"
 
     return (
-        // returning JSX to UI
+        // JSX to UI
         <>
-           <h1> Hello { (n==3)? "Alexa!!":"Priyanshu" } </h1>
-            
+            <h1>Hello {(loggedIn)? lastname || first :"User"}</h1>
+             {/* since it is show the last truly value so we put 2nd para in last parameter */}
+            <p>{ loggedIn && "Welcome Alexa to your profile"  }</p>            
         </>
     );
 }
