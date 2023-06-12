ccb1bac413ed737a71bd5b3f60791ed54ff2a0b3
Priyanshu Kumar Singh <pks9918705@gmail.com>
2023-06-12 09:28:29 +0530

    useState()

---
+++ Ch05 Hooks/hooks/src/Components/InputWithClass.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+export default class Input extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      lastName: ''
+    };
+  }
+
+  nameHandler = (e) => {
+    this.setState({
+      name: e.target.value
+    });
+  };
+
+  lastNameHandler = (e) => {
+    this.setState({
+      lastName: e.target.value
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <div className="section">
+          <Row label="Name">
+            <input onChange={this.nameHandler} value={this.state.name} className="input" />
+          </Row>
+          <Row label="Last Name">
+            <input onChange={this.lastNameHandler} value={this.state.lastName} className="input" />
+          </Row>
+        </div>
+
+        <h2>Hello, {this.state.name} {this.state.lastName}</h2>
+      </>
+    );
+  }
+}
+
+function Row(props) {
+  const { label } = props;
+  return (
+    <>
+      <label>{label}<br /></label>
+      {props.children}
+      <hr />
+    </>
+  );
+}
---
+++ Ch05 Hooks/hooks/src/Components/InputWithFunction.js
@@ -0,0 +1,36 @@
+import {useState} from 'react'
+
+export default function Input(){
+
+    //using stateHook
+    const [name,setName]=useState("")
+    const [lastName,setLastName]=useState("")
+   
+    return(
+        <>
+        <div className="section">
+            <Row label="Name"> 
+                    <input value={name} onChange={(e)=>setName(e.target.value)} className="input"/>
+            </Row >
+            <Row label="Last Name">
+                    <input value={lastName} onChange={(e)=>setLastName(e.target.value)}className="input"/>
+            </Row >
+        </div>
+
+        <h2>Hello, {name} {lastName} </h2>
+        
+        </>
+        )
+    }
+
+
+function Row(props){
+    const{label} = props;
+    return(
+        <>
+        <lable>{label}<br/></lable>
+        {props.children}
+        <hr />
+        </>
+    )
+}
