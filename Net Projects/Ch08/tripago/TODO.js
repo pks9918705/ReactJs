f06548ea7b8fda9bd1ae4d411bd31f531452957c
Priyanshu Kumar Singh <pks9918705@gmail.com>
2023-07-13 22:51:56 +0530

    DB is added

---
+++ Net Projects/Ch08/tripago/TODO.js
@@ -0,0 +1,8 @@
+//TODO: install json server
+//TODO: data folder and put json there
+//TODO: db.json
+//TODO: create a TripList component
+//TODO: use Fetch Api
+//TODO: put it in store and see the component behaviour
+//TODO: what is side effect?
+//TODO: understand useEffect()
